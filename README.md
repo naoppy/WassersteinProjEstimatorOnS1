79eebc50757be13fc1f5f9dd706895e3fb8a0e9e
Otani Naoki <o.naoki0302@gmail.com>
2024-10-30 16:56:06 +0900

    WIP 最尤推定の実装

--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 フォンミーゼス分布や巻き込みコーシー分布などの超球上の分布に対してMLEとW-estimatorの比較をする
 
 ## von Mises Distribution
-平均パラメータ mu と尖度パラメータ beta をもつ。
+平均パラメータ `mu` と尖度パラメータ `kappa` をもつ。
 確率密度関数は
-f(theta) = exp{beta cos(theta - mu)} / (2pi I_0(beta))
+```
+f(theta) = exp{kappa cos(theta - mu)} / (2pi I_0(kappa))
+```
 ここで、I_j は j次の第一種変形ベッセル関数である。
 
+フォンミーゼス分布は指数型分布族に属するので、その理論を応用できる。
+十分統計量は `sin theta` と `cos theta` (の和) である。
+
 ### 最尤推定
+`mu` と `kappa` のどちらも未知のとき、最尤推定量は
+```
+mu = tan^{-1} (sum_i (sin(theta_i)) / sum_i (cos(theta_i)))
+```
+```
+A(kappa)
+:= I_1(kappa) / I_0(kappa)
+ = 1/N(sum_i cos(theta_i)) cos(mu_MLE) + 1/N(sum_i sin(theta_i)) sin(mu_MLE) 
+```
+となる。kappaについては、上記の式を満たす `kappa` を数値的に求めるほかない。
+
+### W-estimator
--- src/main.py
+++ src/main.py
@@ -3,11 +3,40 @@ import numpy as np
 from scipy.stats import vonmises
 
 
+def T(x):
+    """フォンミーゼス分布の十分統計量を返す
+
+    Args:
+        x: フォンミーゼス分布からのサンプル
+
+    Returns:
+        List[float, float]: 十分統計量、[cos, sin] の順
+    """
+    return [np.sum(np.cos(x)), np.sum(np.sin(x))]
+
+
+def MLE(T_data, N: int):
+    """最尤推定を行う
+
+    Args:
+        T_data: 十分統計量
+        N(int): サンプル数
+
+    Returns:
+        Tuple[float, float]: 最尤推定値、[mu_MLE, kappa_MLE] の順
+    """
+    mu_MLE = np.arctan2(T_data[1], T_data[0])
+    target_value = (T_data[0] * np.cos(mu_MLE) + T_data[1] * np.sin(mu_MLE)) / N
+    # TDOO: ここから数値計算で逆関数を求める
+    kappa_MLE = 0
+    return [mu_MLE, kappa_MLE]
+
+
 def main():
     loc = 0.5 * np.pi  # circular mean
     kappa = 1  # concentration
-    sample_size = 1000
-    sample = vonmises(loc=loc, kappa=kappa).rvs(sample_size)
+    N = 1000
+    sample = vonmises(loc=loc, kappa=kappa).rvs(N)
 
     fig = plt.figure(figsize=(12, 6))
     left = plt.subplot(121)
@@ -17,7 +46,7 @@ def main():
     ticks = [0, 0.15, 0.3]
     left.plot(x, vonmises_pdf)
     left.set_yticks(ticks)
-    number_of_bins = int(np.sqrt(sample_size))
+    number_of_bins = int(np.sqrt(N))
     left.hist(sample, density=True, bins=number_of_bins)
     left.set_title("Cartesian plot")
     left.set_xlim(-np.pi, np.pi)
@@ -30,6 +59,8 @@ def main():
     right.legend(bbox_to_anchor=(0.15, 1.06))
 
     plt.show()
+
+    T_data = T(sample)
 
 
 if __name__ == "__main__":
