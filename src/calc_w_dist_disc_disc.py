d47aadcfa858ed1e6e883d2dac9156314f368a1f
Otani Naoki <o.naoki0302@gmail.com>
2024-12-06 01:31:38 +0900

    よりよい例にし、出力を改善した

--- src/calc_w_dist_disc_disc.py
+++ src/calc_w_dist_disc_disc.py
@@ -10,15 +10,17 @@ def pdf_von_Mises(theta, mu, kappa):
 
 
 def print_result(cost, log_dict, p: int):
-    print(f"p: {p}, cost: {cost[0]}, theta: {log_dict['optimal_theta'][0]}")
+    print(
+        f"p: {p}, 100*cost: {100*cost[0]:.6f}, 100*theta: {100*log_dict['optimal_theta'][0]:.3f}"
+    )
     if len(log_dict) != 1:
         print(log_dict)
 
 
 def main():
     t = np.linspace(0, 2 * np.pi, 1000, endpoint=False)
-    n = 100
-    mu1 = 1
+    n = 1000
+    mu1 = 0.1
     kappa1 = 20
     mu2 = 0
     kappa2 = kappa1
@@ -33,14 +35,14 @@ def main():
 
     cost1, log_dict1 = ot.binary_search_circle(x1, x2, p=1, log=True)
     cost2, log_dict2 = ot.binary_search_circle(x1, x2, p=2, log=True)
-    cost3, log_dict3 = ot.binary_search_circle(x1, x2, p=8, log=True)
+    cost3, log_dict3 = ot.binary_search_circle(x1, x2, p=3, log=True)
     print(f"n = {n}")
     print_result(cost1, log_dict1, p=1)
     print_result(cost2, log_dict2, p=2)
-    print_result(cost3, log_dict3, p=8)
+    print_result(cost3, log_dict3, p=3)
 
     # print if you want to see the circular distribution
-    # plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
