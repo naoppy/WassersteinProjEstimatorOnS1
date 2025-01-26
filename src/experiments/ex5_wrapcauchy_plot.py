d8bfb9cf148bfcb93350e16f0a38c9133dd77c4e
Otani Naoki <o.naoki0302@gmail.com>
2025-01-26 22:52:29 +0900

    ex5 強化

--- src/experiments/ex5_wrapcauchy_plot.py
+++ src/experiments/ex5_wrapcauchy_plot.py
@@ -18,7 +18,7 @@ from ..cauchy import MLE_wrapped_cauchy_OKAMURA_method, wrapped_cauchy_cumsum_hi
 from ..plots import brute_heatmap
 
 
-def estimate_param(given_data):
+def est_W1_method2(given_data):
     """与えられたデータから、最適なパラメータを推定する
 
     Args:
@@ -37,20 +37,16 @@ def estimate_param(given_data):
         dist_cumsum_hist = wrapped_cauchy_cumsum_hist.cumsum_hist(mu, rho, bin_num)
         return method2.method2(data_cumsum_hist[1:], dist_cumsum_hist[1:])
 
-    bounds = ((0, 2 * np.pi), (0.01, 0.99))
-    finish_func = partial(optimize.minimize, method="powell", bounds=bounds)
-
     return optimize.brute(
         cost_func,
-        bounds,
+        ((0, 2 * np.pi), (0.01, 0.99)),
         full_output=True,
-        # finish=finish_func,
         finish=None,
         Ns=100,
     )
 
 
-def estimate_param_justopt(given_data):
+def est_W1_method2_justopt(given_data):
     bin_num = len(given_data)
     data_cumsum_hist = wrapped_cauchy_cumsum_hist.cumsum_hist_data(
         given_data, len(given_data)
@@ -61,12 +57,55 @@ def estimate_param_justopt(given_data):
         dist_cumsum_hist = wrapped_cauchy_cumsum_hist.cumsum_hist(mu, rho, bin_num)
         return method2.method2(data_cumsum_hist[1:], dist_cumsum_hist[1:])
 
-    bounds = ((0, 2 * np.pi), (0.01, 0.99))
+    return optimize.minimize(
+        cost_func,
+        (0, 0.5),
+        bounds=((0, 2 * np.pi), (0.01, 0.99)),
+        # for powell method
+        method="powell",
+        options={"xtol": 1e-6, "ftol": 1e-6},
+        # for Nelder-Mead method
+        # method="Nelder-Mead",
+        # options={"xatol": 1e-8, "fatol": 1e-8},
+    )
+
+
+def est_W2_method1(given_data):
+    """Calc W2-estimator using method1"""
+    given_data_norm = np.remainder(given_data, 2 * np.pi) / (2 * np.pi)
+    given_data_norm_sorted = np.sort(given_data_norm)
+
+    def cost_func(x):
+        mu, rho = x
+        sample = stats.wrapcauchy(loc=mu, c=rho).rvs(len(given_data))
+        sample = np.remainder(sample, 2 * np.pi) / (2 * np.pi)
+        sample = np.sort(sample)
+        return method1.method1(given_data_norm_sorted, sample, p=2, sorted=True)
+
+    return optimize.brute(
+        cost_func,
+        ((0, 2 * np.pi), (0.01, 0.99)),
+        full_output=True,
+        finish=None,
+        Ns=100,
+    )
+
+
+def est_W2_method1_justopt(given_data):
+    given_data_norm = np.remainder(given_data, 2 * np.pi) / (2 * np.pi)
+    given_data_norm_sorted = np.sort(given_data_norm)
+
+    def cost_func(x):
+        mu, rho = x
+        sample = stats.wrapcauchy(loc=mu, c=rho).rvs(len(given_data))
+        sample = np.remainder(sample, 2 * np.pi) / (2 * np.pi)
+        sample = np.sort(sample)
+        return method1.method1(given_data_norm_sorted, sample, p=2, sorted=True)
 
     return optimize.minimize(
         cost_func,
         (0, 0.5),
-        bounds=bounds,
+        bounds=((0, 2 * np.pi), (0.01, 0.99)),
         # for powell method
         method="powell",
         options={"xtol": 1e-6, "ftol": 1e-6},
@@ -77,7 +116,7 @@ def estimate_param_justopt(given_data):
 
 
 def main():
-    N = 10000
+    N = 1000
     mu = np.pi / 2
     rho = 0.8
 
@@ -87,19 +126,34 @@ def main():
     sample = stats.wrapcauchy(loc=mu, c=rho).rvs(N)
     sample = np.remainder(sample, 2 * np.pi)
 
-    time1 = time.perf_counter()
+    s_time = time.perf_counter()
     MLE = MLE_wrapped_cauchy_OKAMURA_method.calc_MLE(sample, N, iter_num=100)
-    time2 = time.perf_counter()
-    print(f"MLE result: mu={np.angle(MLE)}, rho={np.abs(MLE)}, time={time2-time1}s")
+    e_time = time.perf_counter()
+    print(f"MLE result: mu={np.angle(MLE)}, rho={np.abs(MLE)}, time={e_time-s_time}s")
 
-    time3 = time.perf_counter()
-    ret = estimate_param(sample)
-    time4 = time.perf_counter()
-    print(f"Mehtod2 Estimation result: time={time4-time3}s")
+    s_time = time.perf_counter()
+    ret = est_W1_method2(sample)
+    e_time = time.perf_counter()
+    print(f"Mehtod2 Estimation result: time={e_time-s_time}s")
     brute_heatmap.plot_heatmap(ret, ("mu", "rho"))
 
-    ret2 = estimate_param_justopt(sample)
+    s_time = time.perf_counter()
+    ret2 = est_W1_method2_justopt(sample)
+    e_time = time.perf_counter()
+    print(f"Mehtod2 Estimation result with just optimization: time={e_time-s_time}s")
     print(ret2)
+
+    s_time = time.perf_counter()
+    ret3 = est_W2_method1(sample)
+    e_time = time.perf_counter()
+    print(f"Mehtod1 Estimation result: time={e_time-s_time}s")
+    brute_heatmap.plot_heatmap(ret3, ("mu", "rho"))
+
+    s_time = time.perf_counter()
+    ret4 = est_W2_method1_justopt(sample)
+    e_time = time.perf_counter()
+    print(f"Mehtod1 Estimation result with just optimization: time={e_time-s_time}s")
+    print(ret4)
 
 
 if __name__ == "__main__":
