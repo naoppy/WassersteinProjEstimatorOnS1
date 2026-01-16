4a552df0510903f93bf549bd4f259c8f57e14e70
Otani Naoki <o.naoki0302@gmail.com>
2026-01-16 18:18:14 +0900

    feat: ex10のもう一つの実験も追加

--- src/experiments/ex10_model_misspecification2.py
+++ src/experiments/ex10_model_misspecification2.py
@@ -1,5 +1,5 @@
 """
-データの生成分布がフォンミーゼス分布のときに、巻き込みコーシー分布でフィッティングした際に平均と円周分散がどうなるかを調べる。
+データの生成分布が巻き込みコーシー分布のときに、フォンミーゼス分布でフィッティングした際に平均と円周分散がどうなるかを調べる。
 MLE, W1, W2で比較。
 つまり、model-misspecificationの意味でのロバスト性を調べる。
 """
@@ -18,17 +18,33 @@ from scipy.stats import vonmises as vonmises_scipy
 from ..calc_semidiscrete_W_dist import method1, method2
 from ..distributions import vonmises, wrapedcauchy
 
-bounds = ((-np.pi, np.pi), (0.01, 0.99))
+bounds = ((-np.pi, np.pi), (0.1, 5))
 
 
-def W2_cost_func3(x, given_data_normed_sorted):
-    sample = wrapedcauchy.quantile_sampling(
-        x[0], x[1], len(given_data_normed_sorted)
-    ) / (2 * np.pi)
-    return method1.method1(given_data_normed_sorted, sample, p=2, sorted=True)
+def Wp_cost_func3(x, given_data_normed_sorted, p: int):
+    sample = vonmises.fast_quantile_sampling(x[0], x[1], len(given_data_normed_sorted))
+    sample = np.remainder(sample, 2 * np.pi) / (2 * np.pi)
+    sample = np.sort(sample)
+    return method1.method1(given_data_normed_sorted, sample, p=p, sorted=True)
 
 
-def est_W2_method3(given_data: npt.NDArray[np.float64]):
+def est_W1_method3(given_data):
+    """calc W1E by method3, given_data should be in [0, 2pi]"""
+    given_data_norm = given_data / (2 * np.pi)
+    given_data_norm_sorted = np.sort(given_data_norm)
+    cost_func = partial(
+        Wp_cost_func3, given_data_normed_sorted=given_data_norm_sorted, p=1
+    )
+    return optimize.minimize(
+        cost_func,
+        (0, 2.5),
+        bounds=bounds,
+        method="powell",
+        options={"xtol": 1e-6, "ftol": 1e-6},
+    )
+
+
+def est_W2_method3(given_data):
     """Calc W2-estimator using method3
 
     Args:
@@ -36,98 +52,92 @@ def est_W2_method3(given_data: npt.NDArray[np.float64]):
     """
     given_data_norm = given_data / (2 * np.pi)
     given_data_norm_sorted = np.sort(given_data_norm)
-    cost_func = partial(W2_cost_func3, given_data_normed_sorted=given_data_norm_sorted)
+    cost_func = partial(
+        Wp_cost_func3, given_data_normed_sorted=given_data_norm_sorted, p=2
+    )
     return optimize.minimize(
         cost_func,
-        (0, 0.5),
+        (0, 2.5),
         bounds=bounds,
         method="powell",
         options={"xtol": 1e-6, "ftol": 1e-6},
     )
 
 
-def W1_method2_cost_func(x, bin_num, data_cumsum_hist):
-    mu, rho = x
-    dist_cumsum_hist = wrapedcauchy.cumsum_hist(mu, rho, bin_num)
+def W2_cost_func1(x, given_data_normed_sorted):
+    sample = vonmises_scipy(loc=x[0], kappa=x[1]).rvs(len(given_data_normed_sorted))
+    sample = np.remainder(sample, 2 * np.pi) / (2 * np.pi)
+    sample = np.sort(sample)
+    return method1.method1(given_data_normed_sorted, sample, p=2, sorted=True)
+
+
+def est_W2_method1(given_data):
+    """method1: random sampling, not used now"""
+    given_data_norm = given_data / (2 * np.pi)
+    given_data_norm_sorted = np.sort(given_data_norm)
+    cost_func = partial(W2_cost_func1, given_data_normed_sorted=given_data_norm_sorted)
+    return optimize.differential_evolution(
+        cost_func, tol=0.01, bounds=bounds, workers=-1, updating="deferred"
+    )
+
+
+def W1_cost_func2(x, bin_num, data_cumsum_hist):
+    mu, kappa = x
+    dist_cumsum_hist = vonmises.cumsum_hist(mu, kappa, bin_num)
     return method2.method2(data_cumsum_hist[1:], dist_cumsum_hist[1:])
 
 
 def est_W1_method2(given_data):
-    """Calc W1-estimator using method1
+    """Calc W1-estimator using method2
 
     Args:
         given_data (np.ndarray): [0, 2*pi]のデータ
     """
     bin_num = len(given_data)
-    data_cumsum_hist = wrapedcauchy.cumsum_hist_data(given_data, bin_num)
+    data_cumsum_hist = vonmises.cumsum_hist_data(given_data, bin_num)
     cost_func = partial(
-        W1_method2_cost_func, bin_num=bin_num, data_cumsum_hist=data_cumsum_hist
+        W1_cost_func2, bin_num=bin_num, data_cumsum_hist=data_cumsum_hist
     )
     return optimize.minimize(
         cost_func,
-        (0, 0.5),
+        (0, 2.5),
         bounds=bounds,
         method="powell",
         options={"xtol": 1e-6, "ftol": 1e-6},
     )
 
 
-def W1_cost_func3(x, given_data_normed_sorted):
-    sample = wrapedcauchy.quantile_sampling(
-        x[0], x[1], len(given_data_normed_sorted)
-    ) / (2 * np.pi)
-    return method1.method1(given_data_normed_sorted, sample, p=1, sorted=True)
-
-
-def est_W1_method3(given_data):
-    """Calc W1-estimator using method3
-
-    Args:
-        given_data (np.ndarray): [0, 2*pi]のデータ
-    """
-    given_data_norm = given_data / (2 * np.pi)
-    given_data_norm_sorted = np.sort(given_data_norm)
-    cost_func = partial(W1_cost_func3, given_data_normed_sorted=given_data_norm_sorted)
-    return optimize.minimize(
-        cost_func,
-        (0, 0.5),
-        bounds=bounds,
-        method="powell",
-        options={"xtol": 1e-6, "ftol": 1e-6},
-    )
-
-
-def run_once(i, true_mu, true_kappa, N: int) -> npt.NDArray[np.float64]:
-    # データはフォンミーゼス分布、モデルは巻き込みコーシー分布
-    sample = stats.vonmises(loc=true_mu, kappa=true_kappa).rvs(N)
+def run_once(i, true_mu, true_rho, N: int) -> npt.NDArray[np.float64]:
+    # データは巻き込みコーシー分布、モデルはフォンミーゼス分布
+    sample = stats.wrapcauchy(loc=true_mu, c=true_rho).rvs(N)
     sample = np.remainder(sample, 2 * np.pi)
 
     s_time = time.perf_counter()
-    MLE = wrapedcauchy.MLE_Kent(sample)
+    MLE = vonmises.MLE(vonmises.T(sample), N)
     e_time = time.perf_counter()
     MLE_mu = MLE[0]
-    MLE_CV = wrapedcauchy.circular_variance(MLE[1])
+    MLE_CV = vonmises.circular_variance(MLE[1])
     MLE_time = e_time - s_time
 
     s_time = time.perf_counter()
     est = est_W1_method2(sample)
     e_time = time.perf_counter()
     W1method2_mu = est.x[0]
-    W1method2_CV = wrapedcauchy.circular_variance(est.x[1])
+    W1method2_CV = vonmises.circular_variance(est.x[1])
     W1method2_time = e_time - s_time
 
     s_time = time.perf_counter()
     est = est_W1_method3(sample)
     e_time = time.perf_counter()
     W1method3_mu = est.x[0]
-    W1method3_CV = wrapedcauchy.circular_variance(est.x[1])
+    W1method3_CV = vonmises.circular_variance(est.x[1])
     W1method3_time = e_time - s_time
 
     s_time = time.perf_counter()
     est = est_W2_method3(sample)
     e_time = time.perf_counter()
     W2method3_mu = est.x[0]
-    W2method3_CV = wrapedcauchy.circular_variance(est.x[1])
+    W2method3_CV = vonmises.circular_variance(est.x[1])
     W2method3_time = e_time - s_time
 
     return np.array(
@@ -149,12 +159,11 @@ def run_once(i, true_mu, true_kappa, N: int) -> npt.NDArray[np.float64]:
 
 
 def main():
-    # true distribution is von Mises
-    true_mu = 0.3
-    true_kappa = 2
-    true_CV = vonmises.circular_variance(true_kappa)
-
-    print(f"true mu={true_mu}, true kappa={true_kappa}, true CV={true_CV}")
+    # true distribution is wraped cauchy
+    true_mu = np.pi / 8
+    true_rho = 0.4
+    true_CV = wrapedcauchy.circular_variance(true_rho)
+    print(f"true mu={true_mu}, true rho={true_rho}, true_CV={true_CV}")
     print("(mu, CV, time)")
 
     log10_Ns = [2, 2.5, 3, 3.5, 4, 4.5, 5]
@@ -193,7 +202,7 @@ def main():
         W2method3_time = np.zeros(try_num)
 
         # MSEをとるための試行回数
-        result = pmap(run_once, range(try_num), (true_mu, true_kappa, N))
+        result = pmap(run_once, range(try_num), (true_mu, true_rho, N))
         for i in range(try_num):
             r = result[i]
             MLE_mu[i] = r[0]
@@ -247,7 +256,7 @@ def main():
         )
 
     print(df)
-    df.to_csv("./data/ex10_model_misspecification_MSE.csv")
+    df.to_csv("./data/ex10_model_misspecification2_MSE.csv")
 
 
 if __name__ == "__main__":
